dfae9bd71dc8e92198600764e41e1f9a9e27341a
LemonNeko <chheese048@gmail.com>
2023-07-20 19:49:07 +0800

    try to improve performance

--- src/main.ts
+++ src/main.ts
@@ -4,85 +4,22 @@ export default function main(origin: HTMLCanvasElement) {
 
   const rawImgData = origin.getContext('2d')!.getImageData(0, 0, originWidth, originHeight).data
 
-  // 旋转 270 度之后的图片
-  const rotatedImgData: number[] = []
-  for (let y = 0; y < originWidth * 4; y += 4) {
-    for (let x = 0; x < originHeight * 4; x += 4) {
-      const position = x * originWidth - y
-      rotatedImgData.push(rawImgData[position], rawImgData[position + 1], rawImgData[position + 2], rawImgData[position + 3])
-    }
-  }
+  let top = origin.height / 2
+  let bottom = origin.height / 2
+  let left = origin.width / 2
+  let right = origin.width / 2
 
-  let top = 0
-  // 检测上方边界
-  for (let y = 0; y < originHeight * 4; y += 4) {
-    for (let x = 0; x < originWidth * 4; x += 4) {
-      const index = y * originWidth + x
-      const r = rawImgData[index];
-      const g = rawImgData[index + 1];
-      const b = rawImgData[index + 2];
-      const a = rawImgData[index + 3];
-      if (r || g || b || a) {
-        // 不是空像素，标记高度
-        console.log(`%c top fist non empty pixel detected, x: ${x / 4}, y: ${y / 4}, r: ${r}, g: ${g}, b: ${b}, a: ${a}`, `background:rgba(${r},${g},${b},${a});color:white`)
-        top = y / 4
-        y = originHeight * 4
-        break
+  for (let y = 0; y < originHeight; y++) {
+    for (let x = 0; x < originWidth; x++) {
+      const a = rawImgData[originWidth * y * 4 + x * 4 + 3]
+      if (a === 0) {
+        continue
       }
-    }
-  }
-  // 检测下方边界
-  let bottom = 0
-  for (let y = originHeight * 4 - 4; y > 0; y -= 4) {
-    for (let x = originWidth * 4 - 4; x > 0; x -= 4) {
-      const index = y * originWidth + x
-      const r = rawImgData[index];
-      const g = rawImgData[index + 1];
-      const b = rawImgData[index + 2];
-      const a = rawImgData[index + 3];
-      if (r || g || b || a) {
-        // 不是空像素，标记高度
-        console.log(`%c bottom fist non empty pixel detected, x: ${x / 4}, y: ${y / 4}, r: ${r}, g: ${g}, b: ${b}, a: ${a}`, `background:rgba(${r},${g},${b},${a});color:white`)
-        bottom = y / 4
-        y = 0
-        break
-      }
-    }
-  }
-  // 检测右侧边界
-  let right = 0
-  for (let y = 0; y < originWidth * 4; y += 4) {
-    for (let x = 0; x < originHeight * 4; x += 4) {
-      const index = y * originHeight + x
-      const r = rotatedImgData[index];
-      const g = rotatedImgData[index + 1];
-      const b = rotatedImgData[index + 2];
-      const a = rotatedImgData[index + 3];
-      if (r || g || b || a) {
-        // 不是空像素，标记高度
-        console.log(`%c right fist non empty pixel detected, x: ${x / 4}, y: ${y / 4}, r: ${r}, g: ${g}, b: ${b}, a: ${a}`, `background:rgba(${r},${g},${b},${a});color:white`)
-        right = originWidth - y / 4
-        y = originWidth * 4
-        break
-      }
-    }
-  }
-  // 检测左侧边界
-  let left = 0
-  for (let y = originWidth * 4 - 4; y > 0; y -= 4) {
-    for (let x = originHeight * 4 - 4; x > 0; x -= 4) {
-      const index = y * originHeight + x
-      const r = rotatedImgData[index];
-      const g = rotatedImgData[index + 1];
-      const b = rotatedImgData[index + 2];
-      const a = rotatedImgData[index + 3];
-      if (r || g || b || a) {
-        // 不是空像素，标记高度
-        console.log(`%c left fist non empty pixel detected, x: ${x / 4}, y: ${y / 4}, r: ${r}, g: ${g}, b: ${b}, a: ${a}`, `background:rgba(${r},${g},${b},${a});color:white`)
-        left = originWidth - y / 4
-        y = 0
-        break
-      }
+
+      if (x < left) { left = x }
+      if (x > right) { right = x}
+      if (y < top) { top = y }
+      if (y > bottom) { bottom = y }
     }
   }
 
